7d67cce628eb57d0b0290dd810cc4fe4608325b2
goodwinj14 <goodwinj14@yahoo.com>
2015-05-20 22:16:44 -0400

    ThreeJS

---
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -0,0 +1,64 @@
+var SCENECOMPONENTS = {};
+var SHAPES = {};
+/*
+*Adds a new 
+*/
+SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, LocationY, LocationZ, shapeID, Scene){
+	//Adds a new cube to our scene based off of the user supplied params
+	if(shape=="Cube"){
+	var cube = null;	
+	cube = new THREE.Mesh(new THREE.CubeGeometry(length, width ,height), new THREE.MeshNormalMaterial());
+	cube.position.set(LocationX,LocationY,LocationZ);
+	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
+		if(cube!=null){
+			Scene.add(cube);
+			SHAPES[shapeID] = cube;
+		}
+	}
+}
+
+SCENECOMPONENTS.move = function(shape_ID, direction, steps){
+	//var temp = shape_ID + ", " + direction + ", " + steps;
+	//We call parseFloat to help javascript tell the difference between the arithmatic + and string concatination +
+	
+	//!!!!!!-----The -0 Helps javascript to recognize the following + opperator as an additon operator and not as a string concatination------!!!!!!  
+	if(direction=="Left"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			shape.position.x = (shape.position.x - steps);
+		}
+	}if(direction=="Right"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			shape.position.x = (shape.position.x-0) + (steps-0);
+		}
+	}if(direction=="Up"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			//!!!!!!-----The -0 Helps javascript to recognize the following + opperator as an additon operator and not as a string concatination------!!!!!!
+			shape.position.y = (shape.position.y-0) + (steps-0);
+		}
+	}if(direction=="Down"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			shape.position.y =  (shape.position.y - steps);
+		}
+	}if(direction=="Forward"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			shape.position.z = (shape.position.z - steps);
+		}
+	}if(direction=="Back"){
+		var shape = null;
+		shape = SHAPES[shape_ID];
+		if(shape!=null){
+			shape.position.z = (shape.position.z-0) + (steps-0);
+		}
+	}
+}
+
---
+++ ThreeJS/server/threeJScontrols/SceneStyles.js
@@ -0,0 +1,15 @@
+
+var SCENESTYLES = {};
+//new THREE.Mesh(new THREE.PlaneGeometry(300,300), new THREE.MeshNormalMaterial());
+
+var geo = new THREE.Geometry();
+var material = new THREE.LineBasicMaterial({color: 'white'});
+var size = 20, step = 1;
+for(var i = -size; i<= size; i+=step){
+	geo.vertices.push(new THREE.Vector3(-size, -0.04, i));
+	geo.vertices.push(new THREE.Vector3(size, -0.04, i));
+	geo.vertices.push(new THREE.Vector3(i, -0.04, -size));
+	geo.vertices.push(new THREE.Vector3(i, -0.04, size));
+}
+
+SCENESTYLES.GRID = new THREE.Line(geo, material, THREE.LinePieces);
