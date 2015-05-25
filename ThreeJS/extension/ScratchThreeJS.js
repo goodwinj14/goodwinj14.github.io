02542decef61b0c77f3a79da11166eda0a96109b
goodwinj14 <goodwinj14@yahoo.com>
2015-05-25 12:31:29 -0400

    ThreeJS

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -26,7 +26,7 @@
 		document.getElementById("faq").innerHTML ='<div bgcolor="#E6E6FA" height="500" width="500"><h3>This is a heading in a div element</h3><p>This is some text in a div element.</p></div>';
 		
 		win = window.open (liveURL, "", "width=window.width, height=window.height");
-		window.addEventListener("keydown", function(e){ console.log(e.key, e.char, e.keyCode) });
+		win.window.addEventListener("keydown", function(e){ console.log(e.key, e.char, e.keyCode) });
         setTimeout(function (){
 			var message = "INIT_"+scene;
 			win.postMessage(message,liveURL);
