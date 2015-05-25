d219e2357d72929a1976a0d6e7bde30f68fd3385
goodwinj14 <goodwinj14@yahoo.com>
2015-05-24 20:28:25 -0400

    ThreeJS

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -22,7 +22,7 @@
 		//Opens the three.js window
 		//win = window.open (liveURL, "", "width=window.width, height=window.height");
 		//Test URLS
-		console.log(document.getElementById("scratch").text);
+		console.log(document.getElementById("scratch"));
 		win = window.open (liveURL, "", "width=window.width, height=window.height");
         setTimeout(function (){
 			var message = "INIT_"+scene;
