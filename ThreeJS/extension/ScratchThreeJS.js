7e86cd4468101b2093491ce1b4d2bcac34ebb716
goodwinj14 <goodwinj14@yahoo.com>
2015-05-25 22:13:31 -0400

    ThreeJS

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -34,7 +34,7 @@
 			console.log(event.data);
 			var key = parseInt(event.data);
 			var evt = document.createEvent("KeyboardEvent");
-  			evt.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, 38, 0);
+  			evt.initKeyboardEvent("keydown", true, true, document.defaultView, false, false, false, false, 0, 38);
     		document.dispatchEvent(evt);
     	}
 		win = window.open (liveURL, "", "width=window.width, height=window.height");
