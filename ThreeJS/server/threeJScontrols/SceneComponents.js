b43659c708e2e3423254e07787ae03a33eb5cb55
goodwinj14 <goodwinj14@yahoo.com>
2015-05-29 12:10:16 -0400

    Sphere Fix

--- ThreeJS/server/threeJScontrols/SceneComponents.js
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -37,7 +37,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	circle.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
 		if(circle!=null){
-			scene.add(circle);
+			Scene.add(circle);
 			SHAPES[shapeID] = circle;
 		}
 	}		
