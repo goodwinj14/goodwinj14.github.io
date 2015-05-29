bc4551b97aca984f601e1fed14981c46a0e2058c
goodwinj14 <goodwinj14@yahoo.com>
2015-05-29 11:39:20 -0400

    Sphere Fix

--- ThreeJS/server/threeJScontrols/SceneComponents.js
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -22,7 +22,7 @@ SCENECOMPONENTS.addShape = function(shape, length, width, height, LocationX, Loc
 	sphere = new THREE.Mesh(new THREE.SphereGeometry(length, width ,height), new THREE.MeshNormalMaterial());
 	sphere.position.set(LocationX,LocationY,LocationZ);
 	//If the shape is created succesfully then it is added to the scene and to to the hash table with its key pointing to that specific object.
-		if(cube!=null){
+		if(sphere!=null){
 			Scene.add(sphere);
 			SHAPES[shapeID] = sphere;
 		}
