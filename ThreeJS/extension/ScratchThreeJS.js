350e9d083b69ed15a8756ca9c7f482bb9a08aa3d
goodwinj14 <goodwinj14@yahoo.com>
2015-05-30 17:10:47 -0400

    Sphere Fix

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -103,7 +103,7 @@
        if (lastKeyEvent == key) {
            lastKeyEvent = null;
            console.log(key);
-           Podium.keydown(83);
+           Podium.keydown(65);
            return true;
        }
 
