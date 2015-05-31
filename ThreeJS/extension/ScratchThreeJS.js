4f17b5fb8cfdfc21a24ccf7f9fa6e0648c15f587
goodwinj14 <goodwinj14@yahoo.com>
2015-05-30 23:25:50 -0400

    Sphere Fix

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -140,7 +140,7 @@
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['w', 'New 3D World %m.Scenes %m.Camera', 'initWorld', "Scene", "Camera Type", ext],
-            ['', 'Set Camera Controls Move Up: %m.Keys Down: %m.Keys Left: %m.Keys Right: %m.Keys ', 'camControlsMove', "w", "s","a","d"],
+            //['', 'Set Camera Controls Move Up: %m.Keys Down: %m.Keys Left: %m.Keys Right: %m.Keys ', 'camControlsMove', 'w', 's','a','d'],
 			//The camera rotate block to allow users to rotate the view of the camra "Left", "Right", "Up" and "Down"
 			['', "Rotate Camera %m.CameraRotation %n Degrees" , 'rotateCamera', "Direction", "1"],
 			//The camera orbit block to allow users to orbit the camera around a given point
