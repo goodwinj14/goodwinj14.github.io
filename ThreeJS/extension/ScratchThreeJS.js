2d5dd8aa29983aed800d90c2c9526fe16024c97a
goodwinj14 <goodwinj14@yahoo.com>
2015-05-30 23:20:48 -0400

    Sphere Fix

--- ThreeJS/extension/ScratchThreeJS.js
+++ ThreeJS/extension/ScratchThreeJS.js
@@ -88,13 +88,8 @@
 		}
 	}
 
-	ext.camControlsMove = function(X, Y, Z){
-		//Makes sure that the shape we are trying to move has been created
-		//console.log(shapes.indexOf(shape_id));
-		if(shapes.indexOf(shape_id)>-1){
-		var message = "MOVESHAPE_"+shape_id+','+direction+','+steps;
-		win.postMessage(message, liveURL);
-		}
+	ext.camControlsMove = function(up, down, left, right){
+		
 	}
 	ext.key_Pressed = function(key) {
        // Reset alarm_went_off if it is true, and return true
@@ -145,7 +140,7 @@
         blocks: [
             // Block type, block name, function name, param1 default value, param2 default value
             ['w', 'New 3D World %m.Scenes %m.Camera', 'initWorld', "Scene", "Camera Type", ext],
-            ['', 'Set Camera Controls Move Up: %m.Keys Down: %m.Keys' 'Left: %m.Keys ''Right: %m.Keys ', 'camControlsMove', "X", "Y",""],
+            ['', 'Set Camera Controls Move Up: %m.Keys Down: %m.Keys' 'Left: %m.Keys ''Right: %m.Keys ', 'camControlsMove', "w", "s","a","d"],
 			//The camera rotate block to allow users to rotate the view of the camra "Left", "Right", "Up" and "Down"
 			['', "Rotate Camera %m.CameraRotation %n Degrees" , 'rotateCamera', "Direction", "1"],
 			//The camera orbit block to allow users to orbit the camera around a given point
