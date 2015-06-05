49a93c20b7661ebd5404e44e4b00a68473455aaa
goodwinj14 <goodwinj14@yahoo.com>
2015-06-05 19:14:54 -0400

    url block

--- ThreeJS/server/threeJScontrols/SceneComponents.js
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -137,7 +137,7 @@ SCENECOMPONENTS.addOBJ = function(url, objID, Scene){
 
        	 });
        	console.log("LOADed Object: ", obj);
-       obj.scale(0.5,0.5,0.5);
+       obj.scale.set(.7,.7,.7);
        Scene.add(obj);
        SHAPES[objID] = obj;
        });
