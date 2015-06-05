c7786ffde48b1836fced5961b3ce428f3f59d93c
goodwinj14 <goodwinj14@yahoo.com>
2015-06-05 18:56:36 -0400

    url block

--- ThreeJS/server/threeJScontrols/SceneComponents.js
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -127,7 +127,7 @@ SCENECOMPONENTS.addOBJ = function(url, objID, Scene){
 
 			//Loads at .OBJ file from an outside url and adds it to the sceen
 			var objLoader = new THREE.OBJLoader();
-			var material = new THREE.MeshNormalMaterial();
+			var material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
     		//var material = new THREE.MeshBasicMaterial({color: 'yellow', side: THREE.DoubleSide});
     		objLoader.load(url, function (obj) {
        		obj.traverse(function (child) {
