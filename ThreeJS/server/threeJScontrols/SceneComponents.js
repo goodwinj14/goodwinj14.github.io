6a217aa494f401fd1e0f0f2940e32c2e2f4cabe2
goodwinj14 <goodwinj14@yahoo.com>
2015-07-16 19:52:04 -0400

    grid

--- ThreeJS/server/threeJScontrols/SceneComponents.js
+++ ThreeJS/server/threeJScontrols/SceneComponents.js
@@ -205,7 +205,7 @@ SCENECOMPONENTS.move = function(shape_ID, direction, steps){
 		shape = SHAPES[shape_ID];
 		if(shape!=null){
 			shape.position.z = (shape.position.z - steps);
-			console.log(SHAPES[shape_ID]);
+			shape.play('walk');
 		}
 	}if(direction=="Back"){
 		var shape = null;
