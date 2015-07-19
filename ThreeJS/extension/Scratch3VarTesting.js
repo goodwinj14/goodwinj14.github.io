e44fc2c6a606d693c93aa26c9e24495c7eae8d07
goodwinj14 <goodwinj14@yahoo.com>
2015-07-19 17:02:28 -0400

    grid

--- ThreeJS/extension/Scratch3VarTesting.js
+++ ThreeJS/extension/Scratch3VarTesting.js
@@ -1,13 +1,4 @@
 (function(ext) {
-	var win = null;
-	var canvas = null;
-	var ctx = null;
-	var lastKeyEvent = null;
-	//var liveURL = "http://localhost:8888/main.html";
-	var shapes = [];
-	var charecters = [];
-	//var liveURL = "http://033ae09.netsolhost.com//gsd2014team5/Localhost/main.html";
-	var liveURL = "http://goodwinj14.github.io/ThreeJS/server/threemain.html";
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
 
@@ -15,22 +6,20 @@
     // Use this to report missing hardware, plugin or unsupported browser
     ext._getStatus = function() {
         return {status: 2, msg: 'Ready'};
-    };    
+    };
 
-	// Block and block menu descriptions
+    ext.my_first_block = function() {
+        // Code that gets executed when the block is run
+    };
+
+    // Block and block menu descriptions
     var descriptor = {
         blocks: [
-            // Block type, block name, function name, param1 default value, param2 default value
-            ['w', 'New 3D World %m.Scenes Width: %n Height: %n %m.Camera', 'initWorld', "Scene", 10, 10 , "Camera Type",ext],
-      ],
-		
-		menus: {
-		        Scenes: ['Grid','Grass','Blank'],
-				Camera: ['Perspective'],
-		    }
+            // Block type, block name, function name
+            [' ', 'my first block', 'my_first_block'],
+        ]
     };
 
     // Register the extension
-   ScratchExtensions.register('Scratch Three JS', descriptor, ext);
+    ScratchExtensions.register('My first extension', descriptor, ext);
 })({});
-
