c536bc7674d43ff5d1c45a0bb19d7bbf71c2d816
goodwinj14 <goodwinj14@yahoo.com>
2015-07-19 18:38:30 -0400

    grid

--- ThreeJS/extension/Scratch3VarTesting.js
+++ ThreeJS/extension/Scratch3VarTesting.js
@@ -1,46 +1,70 @@
-(function(ext) {
+(function() {
+  if (!localStorage.cookieVars) localStorage.cookieVars = '{}';
+  var projectID = Scratch.INIT_DATA ? Scratch.INIT_DATA.PROJECT.model.id : 0,
+    cookieVars = JSON.parse(localStorage.cookieVars)[projectID] || {};
 
-    var items = ["one", "two", "three"];
-    // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {};
+  function DescriptorBuilder(descriptor) {
+    this.descriptor = descriptor;
+  }
 
-    // Status reporting code
-    // Use this to report missing hardware, plugin or unsupported browser
-    ext._getStatus = function() {
-        return {status: 2, msg: 'Ready'};
-    };
+  DescriptorBuilder.prototype.addBlock = function(type, label, op, defaultArgs) {
+    if (!this.descriptor.blocks) this.descriptor.blocks = [];
+    var block = [type, label, op];
+    if (defaultArgs instanceof Array) for (var i = 0, l = defaultArgs.length; i < l; i++) block.push(defaultArgs[i]);
+    this.descriptor.blocks.push(block);
+  };
 
-    ext.my_first_block = function() {
-        //ext._shutdown();
-        //ext2 = {};
-        console.log(ScratchExtensions);
-        //ScratchExtensions.unregister('My first extension');
-        ScratchExtensions.register('My first extension', descriptor2, ext);
+  DescriptorBuilder.prototype.addButton = function(label, action) {
+    if (!this.descriptor.blocks) this.descriptor.blocks = [];
+    this.descriptor.blocks.push([null, label, action]);
+  };
 
-    };
+  DescriptorBuilder.prototype.addSpace = function(height) {
+    if (!this.descriptor.blocks) this.descriptor.blocks = [];
+    if (height === undefined) height = 1;
+    var s = '';
+    for (var i = 0; i < height; i++) s += '-';
+    this.descriptor.blocks.push([s]);
+  };
 
-    // Block and block menu descriptions
-    var descriptor = {
-        blocks: [
-            // Block type, block name, function name
-            [' ', 'my first block %m.Scenes', 'my_first_block'],
-        ],
-        menus: {
-                Scenes: items, 
-            }
-    };
+  DescriptorBuilder.prototype.addMenu = function(name, menu) {
+    if (!this.descriptor.menus) this.descriptor.menus = {};
+    this.descriptor.menus[name] = menu;
+  };
 
-    var descriptor2 = {
-        blocks: [
-            // Block type, block name, function name
-            [' ', 'my first block %m.Scenes', 'my_first_block'],
-            [' ', 'my Second block %m.Scenes', 'my_first_block'],
-        ],
-        menus: {
-                Scenes: items, 
-            }
-    };
+  var extBase = {};
 
-    // Register the extension
-    ScratchExtensions.register('My first extension', descriptor, ext);
-})({});
+  extBase._shutdown = function() {
+    var cookieVarBank = JSON.parse(localStorage.cookieVars);
+    cookieVarBank[projectID] = cookieVars;
+    localStorage.cookieVars = JSON.stringify(cookieVarBank);
+  };
+
+  extBase._getStatus = function() {
+    return {status: 2, msg: 'Ready'};
+  };
+
+
+  function loadExtension() {
+    var ext = Object.create(extBase),
+      varNames = Object.keys(cookieVars),
+      descriptor = {};
+    var db = new DescriptorBuilder(descriptor);
+    db.addButton('Make a Cookie Variable', '');
+   
+      db.addSpace();
+      db.addBlock(' ', 'set %m.cookieVar to %s', 'setCookieVar');
+      db.addBlock(' ', 'change %m.cookieVar by %n', 'changeCookieVar');
+      var cookieVarMenu = [];
+      db.addMenu('cookieVar', cookieVarMenu);
+
+    ScratchExtensions.register('Cookie Variables', descriptor, ext);
+  };
+  
+  function reloadExtension() {
+    ScratchExtensions.unregister('Cookie Variables');
+    loadExtension();
+  }
+
+  loadExtension();
+})();
