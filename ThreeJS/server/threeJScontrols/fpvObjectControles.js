b574eeb67678d91bc8536bc037e74fccec8f6866
goodwinj14 <goodwinj14@yahoo.com>
2015-07-26 14:59:33 -0400

    First Person Controls

--- ThreeJS/server/threeJScontrols/fpvObjectControles.js
+++ ThreeJS/server/threeJScontrols/fpvObjectControles.js
@@ -114,8 +114,8 @@
 
 		//X,Y,Z movment Controles
 		var actualMoveSpeed = delta * this.moveSpeed;
-		if ( this.moveForward ) this.object.translateZ( -actualMoveSpeed );
-		if ( this.moveBackward ) this.object.translateZ( actualMoveSpeed );
+		if ( this.moveForward ) {this.object.translateZ( -actualMoveSpeed );}
+		if ( this.moveBackward ){this.object.translateZ( actualMoveSpeed );}
 
 		if ( this.moveUp ) this.object.translateY( actualMoveSpeed );
 		if ( this.moveDown ) this.object.translateY( - actualMoveSpeed );
