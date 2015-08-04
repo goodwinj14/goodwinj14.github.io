88e69796785b2e6fed16961c4b42b51ef2109219
goodwinj14 <goodwinj14@yahoo.com>
2015-08-04 17:45:33 -0400

    data

--- ThreeJS/server/threeJScontrols/SceneStyles.js
+++ ThreeJS/server/threeJScontrols/SceneStyles.js
@@ -2,15 +2,24 @@
 var SCENESTYLES = {};
 //new THREE.Mesh(new THREE.PlaneGeometry(300,300), new THREE.MeshNormalMaterial());
 
+
+SCENESTYLES.gridSize = 20;
+
+
+SCENESTYLES.GRID = function(){
 var geo = new THREE.Geometry();
 var material = new THREE.LineBasicMaterial({color: 'white'});
-SCENESTYLES.gridSize = 20;
-var step = 1;
-for(var i = -SCENESTYLES.gridSize; i<= SCENESTYLES.gridSize; i+=step){
+	
+
+	var step = 1;
+	for(var i = -SCENESTYLES.gridSize; i<= SCENESTYLES.gridSize; i+=step){
 	geo.vertices.push(new THREE.Vector3(-SCENESTYLES.gridSize, -0.04, i));
 	geo.vertices.push(new THREE.Vector3(SCENESTYLES.gridSize, -0.04, i));
 	geo.vertices.push(new THREE.Vector3(i, -0.04, -SCENESTYLES.gridSize));
 	geo.vertices.push(new THREE.Vector3(i, -0.04, SCENESTYLES.gridSize));
-}
+	}
 
-SCENESTYLES.GRID = new THREE.Line(geo, material, THREE.LinePieces);
+
+	var line = new THREE.Line(geo, material, THREE.LinePieces);
+	return line;
+}
