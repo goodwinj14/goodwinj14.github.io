831694f1ae7019136a23ef3023cc35656b8f36a8
goodwinj14 <goodwinj14@yahoo.com>
2015-08-08 10:30:29 -0400

    data

--- ThreeJS/server/threeJScontrols/threex.planets-master/threex.planets.js
+++ ThreeJS/server/threeJScontrols/threex.planets-master/threex.planets.js
@@ -319,13 +319,13 @@ THREEx.Planets.createPluto	= function(){
 	return mesh	
 }
 
-THREEx.Planets.createStarfield	= function(){
+THREEx.Planets.createStarfield	= function(size){
 	var texture	= THREE.ImageUtils.loadTexture(THREEx.Planets.baseURL+'images/galaxy_starfield.png')
 	var material	= new THREE.MeshBasicMaterial({
 		map	: texture,
 		side	: THREE.BackSide
 	})
-	var geometry	= new THREE.SphereGeometry(100, 32, 32)
+	var geometry	= new THREE.SphereGeometry(size, 32, 32)
 	var mesh	= new THREE.Mesh(geometry, material)
 	return mesh	
 }
