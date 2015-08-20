ed05fbd66353ba263a5845095b1c4cc4c148dc19
goodwinj14 <goodwinj14@yahoo.com>
2015-08-19 20:30:51 -0400

    Blocks UX change

--- Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
+++ Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
@@ -436,7 +436,7 @@ SCENECOMPONENTS.setMaterialTo = function(materialID, objectID, scene){
 			temp = new Physijs.BoxMesh( object.geometry, material,2);
 			//scene.add(temp);
 			//scene.getObjectById(object.id) = temp;
-			console.log("Physics Material: ", object.id);
+			console.log("Physics Material: ", scene.getObjectById(object.id), true);
 			//object = 
 			material = MATERIALS[materialID] = material;
 			
