60496be55ccbfcb1769a8a2127a6d5b669a67ac5
goodwinj14 <goodwinj14@yahoo.com>
2015-08-23 18:07:28 -0400

    Blocks UX change

--- Scratch3D_Beta/server/Physics/CollisionRayCaster.js
+++ Scratch3D_Beta/server/Physics/CollisionRayCaster.js
@@ -1,8 +1,8 @@
-Collisons = {};
+CollisionDetection = {};
 
 
 //The update function checks to see if there has been a change in the touch state of any of
 //the objects in it list of objects
-Collisons.update = function(){
+CollisionDetection.update = function(){
 	console.log("updating caster");
 }
