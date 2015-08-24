469d362ec2094b1676e307fb2b727587c2b7464c
goodwinj14 <goodwinj14@yahoo.com>
2015-08-24 19:39:02 -0400

    Blocks UX change

--- Scratch3D_Beta/server/Physics/CollisionRayCaster.js
+++ Scratch3D_Beta/server/Physics/CollisionRayCaster.js
@@ -17,13 +17,18 @@ CollisionDetection.RayCaster = function(caster, casterID, touch, touchID){
 	this.mesh = caster;
 	//The objects we want to test if the mesh is touching
 	this.conntactObjects = [];
+	this.conntactObjects.push(touch);
 	//The object ID that is associated with the mesh provided
 	this.ID = casterID;
 	//The actual ray caster for this object
 	this.caster = new THREE.Raycaster();
+	this.caster.far = 32;
 	//The rays that will be projected from the origin point when we test for colisions
 	this.rays = this.mesh.geometry.vertices;
+	//Sets the rays to the caster
+	this.caster.set(this.mesh.position, this.rays[i]);
 
+	console.log(this.caster.intersectObjects(this.conntactObjects));
     //Sets up the raycaster far 
     console.log("Caster Object", this.rays);
 
