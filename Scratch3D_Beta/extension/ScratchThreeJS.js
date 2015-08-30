49baab32e665a76f01a644e539ed2744417cd6c8
goodwinj14 <goodwinj14@yahoo.com>
2015-08-30 15:48:35 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/ScratchThreeJS.js
+++ Scratch3D_Beta/extension/ScratchThreeJS.js
@@ -279,7 +279,7 @@
   }
 
   ext.isTouching = function(objectIdOne, ObjectIdTwo){
-    
+    console.log("ext.isTouching");
       if(raycasters[objectIdOne]!=null){
         if(raycasters[objectIdOne].indexOf(ObjectIdTwo)>=0){
           if(collisions[objectIdOne].indexOf(ObjectIdTwo)>=0){
