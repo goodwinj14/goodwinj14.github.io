582ed906ef805ce6dd41e791b0fa9844960abe54
goodwinj14 <goodwinj14@yahoo.com>
2015-08-29 21:42:29 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/ScratchThreeJS.js
+++ Scratch3D_Beta/extension/ScratchThreeJS.js
@@ -284,7 +284,10 @@
   }
       if(raycasters[objectIdOne]!=null){
         if(raycasters[objectIdOne].indexOf(ObjectIdTwo)>=0){
+          if(collisions[objectIdOne].indexOf(ObjectIdTwo)>=0){
            return true;
+          }
+
         }else{
           //add ObjectIdTwo to ObjectIdOne's raycaster checking
           raycasters[objectIdOne].push(ObjectIdTwo);
