df1bfcda97c288da4d4db50a4aa1608849b6fbd5
goodwinj14 <goodwinj14@yahoo.com>
2015-09-01 20:17:23 -0400

    Blocks UX change

--- Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
+++ Scratch3D_Beta/server/threeJScontrols/SceneComponents.js
@@ -450,7 +450,7 @@ SCENECOMPONENTS.setWeight = function(objectID, weight){
 	var shape = SHAPES[objectID];
 	if(shape._physijs != null){
 			if(shape!=null){
-				shape._physijs.mass = weight;
+				shape._physijs.mass = 0.0;
 				console.log(shape);
 				SHAPES[objectID] = shape;
 		}
