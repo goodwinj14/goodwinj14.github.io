3d4ee1343641713d34d9bc75cff49e166fde8ac1
goodwinj14 <goodwinj14@yahoo.com>
2015-09-03 20:27:12 -0400

    pages update

--- Scratch3D_Beta/server/mouseData.js
+++ Scratch3D_Beta/server/mouseData.js
@@ -5,5 +5,10 @@ self.addEventListener('message', function(e) {
 }, false);
 
 function init(){
+	self.addEventListener("click", click(), false);
 	self.postMessage("A successfull web worker call");
+}
+
+function click(e) {
+    self.postMessage("Click From Web Worker");
 }
