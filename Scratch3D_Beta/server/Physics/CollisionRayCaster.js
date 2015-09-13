4034e95ac76ce7bfbbe85b478b09a6cf21ce1bf9
goodwinj14 <goodwinj14@yahoo.com>
2015-09-13 00:01:13 -0400

    Blocks UX change

--- Scratch3D_Beta/server/Physics/CollisionRayCaster.js
+++ Scratch3D_Beta/server/Physics/CollisionRayCaster.js
@@ -76,11 +76,11 @@ CollisionDetection.RayCaster = function(caster, casterID, touch, touchID){
 	this.name = casterID;
 	//The actual ray caster for this object
 	this.caster = new THREE.Raycaster();
-	this.caster.far = .2;
+	this.caster.far = .3;
 	//The rays that will be projected from the origin point when we test for colisions
 	this.rays = [
-	  new THREE.Vector3(1,0, 0),
-      new THREE.Vector3(-1, 0, 0)
+	  new THREE.Vector3( 1, 0, 0),
+      new THREE.Vector3( -1, 0, 0)
       //new THREE.Vector3(0, 1, 0),
       //new THREE.Vector3(0, -1, 0),
       //new THREE.Vector3(0, 0, 1),
