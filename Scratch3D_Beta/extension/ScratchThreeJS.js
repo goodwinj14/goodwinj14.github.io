220f6f4a7053ef5e6056223811df0add17956899
goodwinj14 <goodwinj14@yahoo.com>
2015-09-14 00:34:44 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/ScratchThreeJS.js
+++ Scratch3D_Beta/extension/ScratchThreeJS.js
@@ -349,9 +349,9 @@
 
     console.log("forceCollsionUpadteSent",forceCollsionUpadteSent);
             if(forceCollsionUpadteSent){
-            while(!forceCollsionUpdateRecieved){
+            //while(!forceCollsionUpdateRecieved){
 
-            }
+            //}
           }
     //Checks to see if the object has been created yet
     if((charecters.indexOf(objectIdOne)>=0||shapes.indexOf(objectIdOne)>=0)&&(charecters.indexOf(ObjectIdTwo)>=0||shapes.indexOf(ObjectIdTwo)>=0)){
