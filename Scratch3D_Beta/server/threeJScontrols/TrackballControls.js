8edbacf84001b10b33f157b62a7fe40a0f1ca77c
goodwinj14 <goodwinj14@yahoo.com>
2015-09-15 23:24:42 -0400

    Blocks UX change

--- Scratch3D_Beta/server/threeJScontrols/TrackballControls.js
+++ Scratch3D_Beta/server/threeJScontrols/TrackballControls.js
@@ -338,12 +338,15 @@ THREE.TrackballControls = function ( object, domElement ) {
 			objectUp = new THREE.Vector3(),
 			pan = new THREE.Vector3(_this.move_X_Distance,_this.move_Y_Distance,_this.move_Z_Distance);
 		return function () {
+			pan.x = _this.move_X_Distance;
+			pan.y = _this.move_Y_Distance;
+			pan.z = _this.move_Z_Distance;
 			console.log("this.MoveCamerar X Val: ", pan);
 			_this.object.position.add( pan );
 			_this.target.add( pan );
-			/*_this.move_X_Distance = 0.0;
+			_this.move_X_Distance = 0.0;
 			_this.move_Y_Distance = 0.0;
-			_this.move_Z_Distance = 0.0;*/
+			_this.move_Z_Distance = 0.0;
 		}
 
 	}());
