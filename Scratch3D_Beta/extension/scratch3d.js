9a45b333201c3557bde0c7e20a7cdfd0052b87a5
goodwinj14 <goodwinj14@yahoo.com>
2015-09-27 20:52:12 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -1,3 +1,47 @@
+/***********************************************************/
+/***********************************************************/
+/*****************Html Three Js Code ***********************/
+var htmlCode = "<html>
+    <head>
+        <title>My first Three.js app</title>
+        <style>
+            body { margin: 0; }
+            canvas { width: 100%; height: 100% }
+        </style>
+    </head>
+    <body>
+        <script src='/js/three.min.js'></script>
+        <script>
+            var scene = new THREE.Scene();
+            var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+            var renderer = new THREE.WebGLRenderer();
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.body.appendChild( renderer.domElement );
+
+            var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+            var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+            var cube = new THREE.Mesh( geometry, material );
+            scene.add( cube );
+
+            camera.position.z = 5;
+
+            var render = function () {
+                requestAnimationFrame( render );
+
+                cube.rotation.x += 0.1;
+                cube.rotation.y += 0.1;
+
+                renderer.render(scene, camera);
+            };
+
+            render();
+        </script>
+    </body>
+</html>";
+/*****************Html Three Js Code ***********************/
+/***********************************************************/
+/***********************************************************/
 (function(ext) {
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {};
@@ -14,8 +58,8 @@
     };
 
     ext.newShape = function() {
-       window.document.write();
-
+       window.document.write(htmlCode);
+       return "loaded";
     };
 
     // Block and block menu descriptions
