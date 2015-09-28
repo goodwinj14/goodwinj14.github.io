a803924fff3135c694304c4acbfff0459ada1f26
goodwinj14 <goodwinj14@yahoo.com>
2015-09-27 23:35:28 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -3,7 +3,8 @@
 /*****************Html Three Js Code ***********************/
 var sceneWindow = null;
 var loadingDocument = false;
-var htmlCode = "<html><head><title>My first Three.js app</title><style>body{margin:0}canvas{width:100%;height:100%}</style><body><script>"+threeJs+"</script><script>function addShape(){var e=new THREE.BoxGeometry(1,1,1),n=new THREE.MeshBasicMaterial({color:65280}),r=new THREE.Mesh(e,n);scene.add(r)}function animate(){requestAnimationFrame(animate),render()}var scene=new THREE.Scene,camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1e3),renderer=new THREE.WebGLRenderer;renderer.setSize(window.innerWidth,window.innerHeight),document.body.appendChild(renderer.domElement),camera.position.z=5;var render=function(){renderer.render(scene,camera)};render();</script>"
+console.log(threeJs);
+var htmlCode = "<html><head><title>My first Three.js app</title><style>body{margin:0}canvas{width:100%;height:100%}</style><body><script>"+threeJs.toString()+"</script><script>function addShape(){var e=new THREE.BoxGeometry(1,1,1),n=new THREE.MeshBasicMaterial({color:65280}),r=new THREE.Mesh(e,n);scene.add(r)}function animate(){requestAnimationFrame(animate),render()}var scene=new THREE.Scene,camera=new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,.1,1e3),renderer=new THREE.WebGLRenderer;renderer.setSize(window.innerWidth,window.innerHeight),document.body.appendChild(renderer.domElement),camera.position.z=5;var render=function(){renderer.render(scene,camera)};render();</script>"
 
 
 /*****************Html Three Js Code ***********************/
