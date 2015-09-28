d0058466635b1c06b1040c7d3f107fc4a4154932
goodwinj14 <goodwinj14@yahoo.com>
2015-09-27 21:33:11 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -2,6 +2,7 @@
 /***********************************************************/
 /*****************Html Three Js Code ***********************/
 var sceneWindow;
+var loadingDocument = false;
 var htmlCode = "<html><head><title>My first Three.js app</title><style>body{margin: 0;}canvas{width: 100%; height: 100%}</style></head><body><script src='http://threejs.org/build/three.min.js'></script><script>var scene=new THREE.Scene();var camera=new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );var renderer=new THREE.WebGLRenderer();renderer.setSize( window.innerWidth, window.innerHeight );document.body.appendChild( renderer.domElement );var geometry=new THREE.BoxGeometry( 1, 1, 1 );var material=new THREE.MeshBasicMaterial({color: 0x00ff00});var cube=new THREE.Mesh( geometry, material );scene.add( cube );camera.position.z=5;var render=function (){requestAnimationFrame( render );cube.rotation.x +=0.1;cube.rotation.y +=0.1;renderer.render(scene, camera);};render();</script></body></html>";
 /*****************Html Three Js Code ***********************/
 /***********************************************************/
@@ -16,13 +17,18 @@ var htmlCode = "<html><head><title>My first Three.js app</title><style>body{marg
         return {status: 2, msg: 'Ready'};
     };
 
-    ext.my_first_block = function() {
-       sceneWindow = window.open();
+     = function() {
+       
 
     };
 
+    ext.init = function() {
+        sceneWindow = window.open();
+        sceneWindow.document.write(htmlCode);
+    };
+    
     ext.newShape = function() {
-       sceneWindow.document.write(htmlCode);
+       
        return "loaded";
     };
 
@@ -30,7 +36,7 @@ var htmlCode = "<html><head><title>My first Three.js app</title><style>body{marg
     var descriptor = {
         blocks: [
             // Block type, block name, function name
-            [' ', 'my first block', 'my_first_block'],
+            [' ', 'my first block', 'init'],
             ['r', 'New Shape', 'newShape'],
         ]
     };
