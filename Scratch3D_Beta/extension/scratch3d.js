44c3e9cd3ca5ac684bbb2410c4eb57c33d43e5d4
goodwinj14 <goodwinj14@yahoo.com>
2015-09-30 19:06:46 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -74,7 +74,7 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
       fileSelector.setAttribute('type', 'file');
       fileSelector.setAttribute('id', 'file');
       fileSelector.setAttribute('onclick', 'bClicked()');
-      console.log("Pre Modal4: "window);
+      console.log("Pre Modal4: ",window);
       //window.$modal[0].innerHTML.appendChild(fileSelector);
       console.log("Modal: ", window.$modal[0].innerHTML);
       
