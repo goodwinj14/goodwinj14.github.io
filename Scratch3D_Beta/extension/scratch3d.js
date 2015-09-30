e4c685400690a3e56feaddfcdef53f19a39b2c47
goodwinj14 <goodwinj14@yahoo.com>
2015-09-29 23:49:28 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -74,7 +74,7 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
       fileSelector.setAttribute('type', 'file');
       fileSelector.setAttribute('onclick', 'bClicked()');
       document.body.appendChild(fileSelector);
-      documnet.removeChild(getElementById('scratch'));
+      document.removeChild(getElementById('scratch'));
       var evt = document.createEvent("MouseEvents");
       evt.initEvent("click", true, false);
       fileSelector.dispatchEvent(evt);
