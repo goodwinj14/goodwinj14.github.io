acc8b94272f28fff22919b03ca6d535a63570138
goodwinj14 <goodwinj14@yahoo.com>
2015-10-01 21:50:26 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -84,7 +84,7 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
             $element.appendTo(appendTo)
         }
         $element[0] =  htmlCode;
-        console.log("$element2",$element);
+        console.log("element2: ",$element);
         return $element;
     };
 
