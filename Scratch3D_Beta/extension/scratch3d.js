31c6300924c409e829d1d72a616349d1dfb7b839
goodwinj14 <goodwinj14@yahoo.com>
2015-10-02 23:35:21 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -142,6 +142,7 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
 
         $("button", $3dmodal).click(function(e){
             e.preventDefault();
+            $3dmodal.trigger("hide");
             $(document).trigger("modal:exit")
         });
           //document.body.focus();
