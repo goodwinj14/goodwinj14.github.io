a5f0865fa1dcc3f9db16fe261c85324b8efa709f
goodwinj14 <goodwinj14@yahoo.com>
2015-10-02 21:32:12 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -2,6 +2,7 @@
 /***********************************************************/
 /*****************Html Three Js Code ***********************/
 var sceneWindow = null;
+var $3dmodal = null;
 var loadingDocument = false;
 var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body { margin: 0; } canvas { width: 100%; height: 100% } </style> </head> <body> <script src='https://cdnjs.cloudflare.com/ajax/libs/three.js/r72/three.js'></script> <script> var scene = new THREE.Scene(); var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 ); var renderer = new THREE.WebGLRenderer(); renderer.setSize( window.innerWidth, window.innerHeight ); document.body.appendChild( renderer.domElement ); function addShape(){var geometry = new THREE.BoxGeometry( 1, 1, 1 ); var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } ); var cube = new THREE.Mesh( geometry, material ); scene.add( cube ); } camera.position.z = 5; var render = function () {requestAnimationFrame( render ); renderer.render(scene, camera); }; addShape(); render(); </script> </body> </html>";
 
@@ -130,8 +131,9 @@ var htmlCode = "<html> <head> <title>My first Three.js app</title> <style> body 
         console.log("returned1");
         return $modal;
     }
-
-          $modal = sModal("template-warning", null);
+        if($3dmodal){
+          $3dmodal = sModal("template-warning", null);
+      }
           inputElement = document.getElementById("upfile");
           inputElement.addEventListener("change", function(event){
             file = inputElement.files,
