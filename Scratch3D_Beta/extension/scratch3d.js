9c47582b514e903082cf1512437eed97fb8ef78b
goodwinj14 <goodwinj14@yahoo.com>
2015-10-07 00:30:53 -0400

    Blocks UX change

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -87,7 +87,7 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
         var templateContent = "";
             
             templateContent += $(document.getElementById(templateId)).html();
-                templateContent = "<dialog class='modal-inner' style='height: 400px; width: 500px;'><section id='Holder' style='background-color: #cbffcc; padding: 0px; margin: 0px;'><div id='container' style='height: 360px; width: 500px; background-color: #ccffcc; margin: 45px;'></div></section></dialog>";
+                templateContent = "<dialog class='modal-inner' style='height: 400px; width: 500px;'><section id='Holder' style='background-color: #cbffcc; padding: 0px; margin: 0px;'><div id='container' style='height: 360px; width: 500px; background-color: #ccffcc; margin: 55px;'></div></section></dialog>";
                 //templateContent = htmlCode;
 
         $template = _.template(templateContent);
