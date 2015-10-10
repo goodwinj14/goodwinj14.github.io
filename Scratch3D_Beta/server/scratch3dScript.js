469ecc4e9942cd2acccba6e35b897b7490bdfc84
goodwinj14 <goodwinj14@yahoo.com>
2015-10-10 17:29:20 -0400

    Blocks UX change

--- Scratch3D_Beta/server/scratch3dScript.js
+++ Scratch3D_Beta/server/scratch3dScript.js
@@ -1,35 +1,24 @@
 var Scratch3d = {}
-Scratch3d.init = function(){
-			var scene = new THREE.Scene();
-			var cube = null;
-			var rot = false;
-			var camera = new THREE.PerspectiveCamera( 75, document.getElementById('container').offsetWidth/document.getElementById('container').offsetHeight, 0.1, 1000 );
+/***************************************************************************
+The Base Global attributes that all projects have.
+****************************************************************************/
+var scene;
+var renderer;
+var camera;
 
-			var renderer = new THREE.WebGLRenderer();
+Scratch3d.init = function(){
+			
+			scene = new THREE.Scene();
+			renderer = new THREE.WebGLRenderer();
+			camera = new THREE.PerspectiveCamera( 75, document.getElementById('container').offsetWidth/document.getElementById('container').offsetHeight, 0.1, 1000 );
 			renderer.setSize(document.getElementById('container').offsetWidth,document.getElementById('container').offsetHeight);
 			document.getElementById('container').appendChild( renderer.domElement );
-
-
-			var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-			var material = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
-			cube = new THREE.Mesh( geometry, material );
-			scene.add( cube );
-			cube.rotation.y = 90;
-			cube.rotation.x = 90;
-			camera.position.z = 5;
-			camera.position.y = 1;
-			camera.rotation.x = ( Math.PI / 180);
 			
-			function rotate(){
-				rot = true;
-			};
+			console.log("Scene", scene);
 			var render = function () {
 				requestAnimationFrame( render );
-				if(rot){
-				cube.rotation.x += 0.01;
-				}
+
 				renderer.render(scene, camera);
 			};
 			render();
-			rotate();
 }
