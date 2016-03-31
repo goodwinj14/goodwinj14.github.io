15805d98f0699e201a6378414e7d2fcdbc500f43
goodwinj14 <goodwinj14@yahoo.com>
2016-03-31 16:06:11 -0700

    Update scratch3d.js

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -90,7 +90,7 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
         var templateContent = "";
             
             templateContent += $(document.getElementById(templateId)).html(); 
-                templateContent = "<dialog id='main' class='modal-inner' style=' position: absolute; top: 70px; height: 400px; width: 500px;'><section id='Holder' style='background-color: #cbffcc; padding: 0px; margin-top: -22px;'><div id='container' style='height: 361px; width: 482px; background-color: #ccffcc; margin-left: -19px; '><button id='expand'>fullScreen</button></div></section></dialog>";
+                templateContent = "<dialog id='main' class='modal-inner' style=' position: absolute; top: 70px; height: 400px; width: 500px;'><section id='Holder' style='background-color: #cbffcc; padding: 0px; margin-top: -22px;'><div id='container' style='height: 361px; width: 482px; background-color: #ccffcc; margin-left: -19px; '><img src='http://www.w3schools.com/images/w3schools_green.jpg'><button id='expand'>fullScreen</button></div></section></dialog>";
                 //templateContent = htmlCode;
 
         $template = _.template(templateContent);
