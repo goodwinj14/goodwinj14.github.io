b930b5344ce8d139967ed293d1d1e6ca3601254a
goodwinj14 <goodwinj14@yahoo.com>
2016-04-04 20:11:17 -0700

    Update scratch3d.js

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -153,8 +153,8 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
     $("img", $modal).click(function(e){
         if(e.delegateTarget.id=="expand"){
             
-            var w = (window.innerWidth-10).toString() + "px";
-            var h = (window.innerHeight-10).toString() + "px";
+            var w = (window.innerWidth-15).toString() + "px";
+            var h = (window.innerHeight-5).toString() + "px";
             
             if(!fullScreenOn){
             fullScreenOn= true;
