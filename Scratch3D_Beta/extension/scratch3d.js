e7d893796bee5a5cc92662e8513487c197ed370d
goodwinj14 <goodwinj14@yahoo.com>
2016-04-04 17:00:41 -0700

    Update scratch3d.js

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -5,6 +5,7 @@ var sceneWindow = null;
 var $3dmodal = null;
 var loadingDocument = false;
 var htmlCode;
+Var fullScreenOn = false;
 //var htmlCode = "<script>var scene = new THREE.Scene();var cube = null;var camera = new THREE.PerspectiveCamera( 75, document.getElementById('container').offsetWidth/document.getElementById('container').offsetHeight, 0.1, 1000 );var renderer = new THREE.WebGLRenderer();renderer.setSize(document.getElementById('container').offsetWidth,document.getElementById('container').offsetHeight);document.getElementById('container').appendChild( renderer.domElement );var geometry = new THREE.BoxGeometry( 1, 1, 1 );var material = new THREE.MeshbasicMaterial( { color: 0x00ff00 } );cube = new THREE.Mesh( geometry, material );scene.add( cube );cube.rotation.y = 90;cube.rotation.x = 90;camera.position.z = 5;camera.position.y = 1;camera.rotation.x = ( Math.PI / 180);var render = function () {requestAnimationFrame( render );console.log('rendering');if(cube!=null){cube.rotation.x += 0.01;}renderer.render(scene, camera);};//render();}</script>";
 /*****************Html Three Js Code ***********************/
 /***********************************************************/
@@ -151,6 +152,9 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
 
     $("img", $modal).click(function(e){
         if(e.delegateTarget.id=="expand"){
+            
+            if(!fullScreenOn){
+            fullScreenOn= true;
             document.getElementById("main").style.width = "1100px";
             document.getElementById("Holder").style.width = "1100px";
             document.getElementById("container").style.width = "1100px";
@@ -159,6 +163,10 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
             document.getElementById("container").style.height = "700px";
             Scratch3d.updateWindow();
             console.log("Enter Full Screen",e);
+            }else{
+                //Code to min window goes here
+            }
+            
         }else{
         Scratch3d.windowActive = false;
         e.preventDefault();
