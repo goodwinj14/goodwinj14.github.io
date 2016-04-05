d9528463d5f4296928f2cbf06fca1ad1748557f4
goodwinj14 <goodwinj14@yahoo.com>
2016-04-04 20:01:33 -0700

    Update scratch3d.js

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -159,6 +159,7 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
             if(!fullScreenOn){
             fullScreenOn= true;
             document.getElementById("main").style.width = w;
+             document.getElementById("main").style.position.top = "0px";
             document.getElementById("Holder").style.width = w;
             document.getElementById("container").style.width = w;
             document.getElementById("main").style.height = h;
