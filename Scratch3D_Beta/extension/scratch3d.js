49b630be2c6670043af987c032239b7f1c469c0f
goodwinj14 <goodwinj14@yahoo.com>
2016-04-20 18:04:55 -0700

    Update scratch3d.js

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -158,13 +158,13 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
         buttonID = e.delegateTarget.id;
         if(buttonID=='edit_mode'){
             var w = (window.innerWidth-7).toString() + "px";
-            var containerWidth = (window.innerWidth-307).toString() + "px";
+            var containerWidth = (window.innerWidth-407).toString() + "px";
             var h = (window.innerHeight-7).toString() + "px";
             var bPosition = (window.innerWidth-30).toString() + "px";
             fullScreenOn= true;
             document.getElementById("main").style.width = w;
             document.getElementById("main").style.top = "0px";
-            document.getElementById("Holder").style.width = w;
+            document.getElementById("Holder").style.width = containerWidth;
             document.getElementById("container").style.width = containerWidth;
             document.getElementById("main").style.height = h;
             document.getElementById("Holder").style.height = h;
