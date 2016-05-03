d952cb903dddc6391e5745b2693485379b79166e
Mac <macBook@Macs-MacBook-Air.local>
2016-05-02 22:16:55 -0700

    test

--- Scratch3D_Beta/extension/scratch3d.js
+++ Scratch3D_Beta/extension/scratch3d.js
@@ -178,7 +178,7 @@ function CreateFromTemplate(elementId, templateId, elementType, appendTo, wrappe
             document.getElementById("main").style.width = w;
             document.getElementById("main").style.top = "0px";
             document.getElementById("Holder").style.width = w;
-            document.getElementById("container").style.width = "70%";
+            document.getElementById("container").style.width = "73%";
             document.getElementById("main").style.height = h;
             document.getElementById("Holder").style.height = h;
             document.getElementById("container").style.height = h;
