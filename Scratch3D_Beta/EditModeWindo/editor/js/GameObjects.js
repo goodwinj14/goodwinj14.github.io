164ae4d71e06e9a7264a620c7798f27182a55125
Mac <macBook@Macs-MacBook-Air.local>
2016-05-06 19:25:52 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/GameObjects.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/GameObjects.js
@@ -7,9 +7,6 @@ GAME_OBJECTS.Shapes = [];
 GAME_OBJECTS.RemoveObject = function(objectID){
 	//Removes object from scene
 	var removeObject = scene.getObjectByName(objectID);
-	var geo = removeObject.geometry;
-	var mat = removeObject.material;
-	var text = removeObject.texture;
 	//Checks if the object was in the scene and if so it is removed
 	if(removeObject!=undefined){
 		scene.remove( removeObject );
@@ -17,10 +14,10 @@ GAME_OBJECTS.RemoveObject = function(objectID){
 		//In the futur we will also have to remove it from our arrays that store the objects 
 		//It may be in the array but not in the scene
 	}
-	geo.dispose();
-	mat.dispose();
-	text.dispose();
-	removeObject.mesh.dispose(); // new
-
+	removeObject.mesh.dispose(); 
+	removeObject.geometry.dispose();
+	removeObject.material.dispose();
+	removeObject.texture.dispose();
+	
 	
 }
