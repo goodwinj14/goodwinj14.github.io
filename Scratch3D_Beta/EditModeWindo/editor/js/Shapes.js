ee26962b4007bc9f4973bf1436be27509f1d4651
Mac <macBook@Macs-MacBook-Air.local>
2016-05-06 20:39:10 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/Shapes.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/Shapes.js
@@ -99,10 +99,10 @@ function newRing(){
 	return shape;
 }
 
-SHAPES.createFromExisting = function(shape){
+SHAPES.createFromExisting = function(shape_name){
 	var shape;
-	console.log("createFromExisting ");
-	if(shape.indexOf("add_cube") > -1){
+	
+	if(shape_name.indexOf("add_cube") > -1){
 		console.log("new Cube");
 		shape =  newCube();
 		shape.name="cube_"+shape.id;
