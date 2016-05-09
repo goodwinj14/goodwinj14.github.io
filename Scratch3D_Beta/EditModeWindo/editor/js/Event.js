8fb7302b75e2445e1876f3344ba94b03385c733d
Mac <macBook@Macs-MacBook-Air.local>
2016-05-08 21:26:27 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/Event.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/Event.js
@@ -19,7 +19,6 @@ EVENT.init = function(){
 	}, false);
 
 	document.getElementById("canvas").addEventListener("mousemove", function(event){
-		console.log(event);
     MOUSE_LOC_CHANGED = true;
     if(MOVE_OBJ_ACTIVE){
     	//obj_position_change(event);
