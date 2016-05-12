f65f4ed1a6473d089a0ef76efb140a4c569e412b
Mac <macBook@Macs-MacBook-Air.local>
2016-05-12 00:23:57 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
@@ -71,7 +71,7 @@ SELECTED.select = function(event){
 
 	face_0.position.set(boundingBox.max.x,boundingBox.min.y,0);
 	face_1.position.set(boundingBox.min.x,boundingBox.min.y,0);
-	face_2.position.set(boundingBox.min.x*avtive_Object.scale.x,boundingBox.min.y*avtive_Object.scale.y,boundingBox.min.z*avtive_Object.scale.z);
+	face_2.position.set(0,boundingBox.min.y,boundingBox.min.z);
 	right_back_coner.position.set(boundingBox.max.x*avtive_Object.scale.x,boundingBox.min.y*avtive_Object.scale.y,boundingBox.min.z*avtive_Object.scale.z);
 	top_center_coner.position.set(face_1.position.x+((face_0.position.x-face_1.position.x)/2),boundingBox.max.y*avtive_Object.scale.y+size,face_1.position.z+((right_back_coner.position.z-face_0.position.z)/2));
 	avtive_Object.position.x = avtive_Object.position.x+((avtive_Object.scale.x-(boundingBox.max.x-boundingBox.min.x))/2);
