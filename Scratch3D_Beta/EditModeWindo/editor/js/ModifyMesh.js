88581fc9d65ff057cef46bba95dd5a7db608c9b8
BuildTools <unconfigured@null.spigotmc.org>
2016-05-12 15:40:22 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/ModifyMesh.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/ModifyMesh.js
@@ -25,7 +25,7 @@ ModifyMesh.updateDimensions = function(mesh,sideID, dist){
 					vertices[i] = minPoint+(xStepDist*(Math.abs(vertices[i]-minPoint)/length));
 				}
 			}
-			mesh.position.x = mesh.position.x + ((dist.x - length)/2);
+			mesh.position.x = mesh.position.x + ((dist.x-(mesh.position.x+maxPoint))/2);
 		}
 		//Alters the min x width of the object
 		if(sideID=="face_1"){
