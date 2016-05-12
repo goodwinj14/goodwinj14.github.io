de5499df161806f30b1f63997d0ad6ab76d2b5fc
BuildTools <unconfigured@null.spigotmc.org>
2016-05-12 14:03:12 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/editerMain.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/editerMain.js
@@ -30,15 +30,20 @@ EDITOR.intiEditor = function(viewport){
 			var vector = new THREE.Vector3(0,0,0);
 			camera.lookAt(vector);
 
-			controls = new THREE.TrackballControls( camera );
+			controls = new THREE.TrackballControls( camera, viewport );
+
 				controls.rotateSpeed = 1.0;
 				controls.zoomSpeed = 1.2;
 				controls.panSpeed = 0.8;
+
 				controls.noZoom = false;
 				controls.noPan = false;
+
 				controls.staticMoving = true;
 				controls.dynamicDampingFactor = 0.3;
+
 				controls.keys = [ 65, 83, 68 ];
+
 				controls.addEventListener( 'change', render );
 
 
