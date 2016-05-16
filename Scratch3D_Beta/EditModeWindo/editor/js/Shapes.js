8393f9756a7c61358e33d42d5257b574e02566ea
BuildTools <unconfigured@null.spigotmc.org>
2016-05-16 15:46:31 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/Shapes.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/Shapes.js
@@ -97,13 +97,29 @@ geometry.attributes.faces = [
 // create a simple square shape. We duplicate the top left and bottom right
 // vertices because each vertex needs to appear once per triangle.
 var vertices = new Float32Array( [
-	-1.0, -1.0,  1.0,
-	 1.0, -1.0,  1.0,
-	 1.0,  1.0,  1.0,
+	-0.5, 0, -0.5
+	 0.5, 0, -0.5,
+	 -0.5, 0, 0.5,
 
-	 1.0,  1.0,  1.0,
-	-1.0,  1.0,  1.0,
-	-1.0, -1.0,  1.0
+	 -0.5, 0, 0.5,
+	 0.5, 0, -0.5,
+	 0.5, 0, 0.5,
+
+	 -0.5, 0, -0.5,
+	 -0.5, 0, 0.5,
+	 0, 1, 0,
+
+	 0.5, 0, -0.5,
+	 -0.5, 0, -0.5,
+	 0, 1, 0,
+
+	 0.5, 0, 0.5,
+	 0.5, 0, -0.5,
+	 0, 1, 0,
+
+	 -0.5, 0, 0.5,
+	 0.5, 0, 0.5,
+	 2, 3, 4
 ] );
 
 // itemSize = 3 because there are 3 values (components) per vertex
