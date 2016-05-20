378ed9e07a0f511367550d0fdb5e3ac98094e138
BuildTools <unconfigured@null.spigotmc.org>
2016-05-20 15:17:56 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
@@ -60,12 +60,12 @@ SELECTED.init = function(object){
 	rotate_x.rotation.y = Math.PI/2;
 	mat_ring_y = new THREE.MeshBasicMaterial();
 	mat_ring_y.side = THREE.DoubleSide;
-	mat_ring_y.color = rotationX_Color;
+	mat_ring_y.color = rotationY_Color;
 	rotate_y = new THREE.Mesh(new THREE.RingGeometry( 1, 1.05, 64 ), mat_ring_y);
 	rotate_y.rotation.x = Math.PI/2;
 	mat_ring_z = new THREE.MeshBasicMaterial();
 	mat_ring_z.side = THREE.DoubleSide;
-	mat_ring_z.color = rotationX_Color;
+	mat_ring_z.color = rotationZ_Color;
 	rotate_z = new THREE.Mesh(new THREE.RingGeometry( 1, 1.05, 64 ), mat_ring_z);
 	shapeHelpers.add(face_0);
 	shapeHelpers.add(face_1);
