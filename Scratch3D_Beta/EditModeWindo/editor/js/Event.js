2c715589d52c783781f48c5147e6f897d4f2e248
BuildTools <unconfigured@null.spigotmc.org>
2016-05-24 22:21:22 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/Event.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/Event.js
@@ -92,7 +92,7 @@ function objectRotation(e){
 
          var angle = Math.acos(b.dot(c)/(b.length()*c.length() ) );
          angle = ((Math.round(angle*(Math.PI / 180)))*Math.PI)/180;
-         console.log("Degrees", Math.round(angle*(Math.PI / 180)));
+         console.log("Degrees", Math.ceil(angle*(Math.PI / 180)));
 
          if((((b.x-a.x)*(c.y-a.y))- ((b.y-a.y)*(c.x-a.x)))>0){
          	angle = angle*-1;
