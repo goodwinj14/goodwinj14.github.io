7a996e25bba1a3ae078639e9a652202a98567c79
BuildTools <unconfigured@null.spigotmc.org>
2016-05-24 22:23:03 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/Event.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/Event.js
@@ -91,7 +91,7 @@ function objectRotation(e){
          var deltaRotationQuaternion;
 
          var angle = Math.acos(b.dot(c)/(b.length()*c.length() ) );
-         angle = ((Math.round(angle*(Math.PI / 180)))*Math.PI)/180;
+         angle = ((Math.ceil(angle*(Math.PI / 180)))*Math.PI)/180;
          console.log("Degrees", Math.ceil(angle*(Math.PI / 180)));
 
          if((((b.x-a.x)*(c.y-a.y))- ((b.y-a.y)*(c.x-a.x)))>0){
