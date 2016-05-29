3072e0a9fef89fe033055ff2221c4dcae3b0c89b
BuildTools <unconfigured@null.spigotmc.org>
2016-05-29 11:56:27 -0700

    test

--- Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
+++ Scratch3D_Beta/EditModeWindo/editor/js/SelectedElement.js
@@ -79,7 +79,7 @@ SELECTED.init = function(object){
 	rotate_z.name = "rotate_z";
 
 	var dir = new THREE.Vector3( 0, 1, 0 );
-	var origin = new THREE.Vector3( 0, boundingBox.min.y, 0 );
+	var origin = new THREE.Vector3( 0, object.position.y, 0 );
 	var length = 1.4;
 	var hex = 0xffff00;
 
