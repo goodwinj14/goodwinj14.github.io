994cf7119c19001a50e533228e5ae1114aef5fd2
BuildTools <unconfigured@null.spigotmc.org>
2016-05-30 00:07:47 -0700

    test

--- Scratch3D_Beta/EditModeWindo/sidebar/js/sidebar.js
+++ Scratch3D_Beta/EditModeWindo/sidebar/js/sidebar.js
@@ -91,8 +91,10 @@ function updateShape(){
         rotX = rotX-edit_Obj.rotationalOffset.x;
         ModifyMesh.updateRotation(edit_Obj, new THREE.Euler((rotX*Math.PI)/180,0, 0,'XYZ'));
     }if(rotY!=edit_Obj.rotationalOffset.y){
+        rotY = rotY-edit_Obj.rotationalOffset.y;
         ModifyMesh.updateRotation(edit_Obj, new THREE.Euler(0,(rotY*Math.PI)/180, 0,'XYZ'));
     }if(rotZ!=edit_Obj.rotationalOffset.z){
+        rotZ = rotZ-edit_Obj.rotationalOffset.z;
         ModifyMesh.updateRotation(edit_Obj, new THREE.Euler(0,0,(rotZ*Math.PI)/180,'XYZ'));
     }
 
